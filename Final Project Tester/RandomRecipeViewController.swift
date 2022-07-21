199cf5ab598dd3b900aa0b02df79288cc98301b8
Kavya Krishnan <kavyalkrishnan08@gmail.com>
2022-07-21 15:27:41 -0400

    Latest Update(finished generator)

--- Final Project Tester/RandomRecipeViewController.swift
+++ Final Project Tester/RandomRecipeViewController.swift
@@ -15,12 +15,25 @@ class RandomRecipeViewController: UIViewController {
     @IBOutlet weak var image2: UIImageView!
     @IBOutlet weak var recipe2: UILabel!
     
+    
+ 
+    @IBOutlet weak var recipe3: UILabel!
+    @IBOutlet weak var image3: UIImageView!
+    
+    
+    @IBOutlet weak var image4: UIImageView!
+    @IBOutlet weak var recipe4: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         image1.isHidden = true
         recipe1.isHidden = true
         image2.isHidden = true
         recipe2.isHidden = true
+        image3.isHidden = true
+        recipe3.isHidden = true
+        image4.isHidden = true
+        recipe4.isHidden = true
     }
     
 var counter = 0
@@ -36,6 +49,24 @@ var counter = 0
             recipe1.isHidden = true
             image2.isHidden = false
             recipe2.isHidden = false
+        }
+        if counter == 3{
+            image1.isHidden = true
+            recipe1.isHidden = true
+            image2.isHidden = true
+            recipe2.isHidden = true
+            image3.isHidden = false
+            recipe3.isHidden = false
+        }
+        if counter == 4{
+            image1.isHidden = true
+            recipe1.isHidden = true
+            image2.isHidden = true
+            recipe2.isHidden = true
+            image3.isHidden = true
+            recipe3.isHidden = true
+            image4.isHidden = false
+            recipe4.isHidden = false
         }
     }
     
